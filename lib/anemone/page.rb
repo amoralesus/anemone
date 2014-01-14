fd52117799af8069d17a62c5b507986fe248c4dc
moralesalberto <alberto@moralitos.com>
2014-01-14 14:41:59 -0500

    changing the name to uri, since the link is returning a uri

--- lib/anemone/page.rb
+++ lib/anemone/page.rb
@@ -69,10 +69,10 @@ module Anemone
         next if u.nil? or u.empty? or a.text.to_s.empty?
         abs = to_absolute(u) rescue next
         @links << abs if in_domain?(abs)
-        @link_objects << OpenStruct.new(:name => abs, :title => a.text.to_s.gsub(/[\n\t\r]/, "").split(" ").join(" "))
+        @link_objects << OpenStruct.new(:uri => abs, :title => a.text.to_s.gsub(/[\n\t\r]/, "").split(" ").join(" "))
       end
       @links.uniq!
-      @link_objects = @link_objects.uniq { |link| link.name }
+      @link_objects = @link_objects.uniq { |link| link.uri }
       @links
     end
 
